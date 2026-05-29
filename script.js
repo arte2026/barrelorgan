f199b8e363ef4fe74c02a67b982125fdd35d6792
Guillermo Rayas M <bodisatva5@outlook.com>
2026-05-28 19:41:16 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -462,7 +462,7 @@ welcomeScreen.addEventListener("click", () => {
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/service-worker.js")
+      .register("./service-worker.js")
       .then((registration) => {
         console.log("Service Worker registered with scope:", registration.scope);
       })
